f2a141d3dc3d92e3bb02e018a2db9a637c2be5da
George Karan <georgekaran12@gmail.com>
2024-05-29 17:03:40 -0300

    Update reproduction

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,10 @@
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://example.cypress.io');
+
+    cy.contains('Kitchen Sink').should('be.visible');
+
+    cy.document().selectFile('example.json');
   })
 })
